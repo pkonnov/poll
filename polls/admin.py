a1fceacdbbcc2549f6fa89181f18c005f3815a65
cooh <admin@sarinform.ru>
2017-12-13 13:08:35 +0400

    add admin.ModelAdmin

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from .models import Question
 
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [ (None, {'fields': ['question_text']}), 
+	('Date informaton', {'fields':['pub_date'], 'classes':['collapse']}),]
+
+admin.site.register(Question, QuestionAdmin)
 
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,6 @@
 import datetime
-from django.db import models
 from django.utils import timezone
+from django.db import models
 
 
 class Question(models.Model):
@@ -8,7 +8,7 @@ class Question(models.Model):
 	pub_date = models.DateTimeField('date publiched')
 
 	def __str__(self):
-		self.question_text
+		return self.question_text
 
 	def was_publiched_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -20,4 +20,4 @@ class Choice(models.Model):
 	votes = models.IntegerField(default=0)
 
 	def __str__(self):
-		self.choice_text
+		return self.choice_text
