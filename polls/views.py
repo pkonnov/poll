28edf11df60e5c52aca709e3e989835dc5dba339
cooh <admin@sarinform.ru>
2017-12-15 11:02:44 +0400

    https://docs.djangoproject.com/en/2.0/intro/tutorial03/ | 2.5.4 refused RequestCintext

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 
 def index(request):
-	return HttpResponse('Hello, world!!')
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	# output = ', '.join([p.question_text for p in latest_question_list])
+	context = { 'latest_question_list': latest_question_list }
+	return HttpResponse(template.render(context, request))
+
+
+
+def detail(request, question_id):
+	return HttpResponse("You`re voting on question %s" % question_id)
+
+def results(request, question_id):
+	response = "You`re voting on question %s"
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You`re voting on question %s" % question_id)
