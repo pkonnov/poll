f1dded300c6c514d1dc5613e3f21e31a71c9e246
cooh <admin@sarinform.ru>
2017-12-22 12:16:30 +0400

    refactoring

--- polls/views.py
+++ polls/views.py
@@ -1,42 +1,44 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	# output = ', '.join([p.question_text for p in latest_question_list])
-	context = { 'latest_question_list': latest_question_list }
-	return HttpResponse(template.render(context, request))
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]	
 
 
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question}) 
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
 
 
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 
 
 def vote(request, question_id):
-	p = get_object_or_404(Question, pk=question_id)
-	try:
-		selected_choice = p.choice_set.get(pk=request.POST['choice'])
-	except (KeyError, Choice.DoesNotExist):
-		return render(request, 'polls/detail.html', {
-				'question': p,
-				'error_message': "Пожалуйста выберете ответ",
-			})
-	else:
-		selected_choice.votes += 1
-		selected_choice.save()
-		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "Что бы увидеть результат, выбери вариант ответа",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
