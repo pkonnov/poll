01b03afb259425a9c9ffd3947e779f8de9ba5e53
cooh <admin@sarinform.ru>
2017-12-22 11:31:13 +0400

    add form vote

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.db import models
 
 
+
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date publiched')
@@ -18,7 +19,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-	question = models.ForeignKey(Question, 'on_delete')
+	question = models.ForeignKey(Question)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
 
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, render
-from django.template import loader
-from .models import Question
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+
+from .models import Choice, Question
 
 
 
@@ -21,8 +22,21 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-	response = "You`re voting on question %s"
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
+
+
 
 def vote(request, question_id):
-	return HttpResponse("You`re voting on question %s" % question_id)
+	p = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+				'question': p,
+				'error_message': "Пожалуйста выберете ответ",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
